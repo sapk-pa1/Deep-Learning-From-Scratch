2a947148ae94c6ff0d12c948702ac1a3547b78e6
1ap <sapkotapawan099@gmail.com>
2023-11-22 07:23:48 +0545

    first

--- Foundations/utils.py
+++ Foundations/utils.py
@@ -1,5 +1,12 @@
-from typing import Callable 
+from typing import Callable , List 
 from numpy import ndarray 
+
+
+# A Function takes in an ndarray as an argument and produces an ndarray 
+array_function = Callable[[ndarray], ndarray]
+
+# A chain is a list of function 
+Chain = List[array_function]
 
 def deriv(func: Callable[[ndarray], ndarray], 
           input_: ndarray, 
@@ -15,3 +22,22 @@ def deriv(func: Callable[[ndarray], ndarray],
         ndarray: An array containing the derivative values corresponding to each element in the input array.
     """
     return (func(input_ + delta) - func(input_ - delta)) / (2 * delta)
+
+def chain_length_2(chain: Chain, 
+                   a: ndarray) -> ndarray: 
+    """Evaluates two functions in a row, in a Chain 
+
+    Args:
+        chain (Chain): list of functions 
+        a (ndarray): input
+
+    Returns:
+        ndarray: output from appling the input to the chain of functions 
+    """
+    assert len(chain) ==2 
+    # Length of the cchain shoud be 2 
+
+    f1 = chain[0]
+    f2= chain[1]
+
+    return f2(f1(a))
